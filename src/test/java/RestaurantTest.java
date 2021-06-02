c841988fd7ac66079bc42c5f1ef32560c6fbe501
alekhya51292 <alekhya51292@gmail.com>
2021-06-02 21:57:47 +0530

    Part 2: Solution

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.time.LocalTime;
 
@@ -8,17 +9,35 @@ import static org.junit.jupiter.api.Assertions.*;
 class RestaurantTest {
     Restaurant restaurant;
     //REFACTOR ALL THE REPEATED LINES OF CODE
+    @BeforeEach
+    public void setup_Restaurant_Object(){
+        LocalTime openingTime = LocalTime.parse("10:30:00");
+        LocalTime closingTime = LocalTime.parse("22:00:00");
+        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
+    }
 
     //>>>>>>>>>>>>>>>>>>>>>>>>>OPEN/CLOSED<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     //-------FOR THE 2 TESTS BELOW, YOU MAY USE THE CONCEPT OF MOCKING, IF YOU RUN INTO ANY TROUBLE
     @Test
     public void is_restaurant_open_should_return_true_if_time_is_between_opening_and_closing_time(){
         //WRITE UNIT TEST CASE HERE
+        restaurant = Mockito.spy(restaurant);
+        //Mock getCurrentTime() to 10 minutes before the closingTime
+        Mockito.when(restaurant.getCurrentTime()).thenReturn(restaurant.closingTime.minusMinutes(10));
+        Boolean restaurantStatus = restaurant.isRestaurantOpen();
+        assertTrue(restaurantStatus);
     }
 
     @Test
     public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time(){
         //WRITE UNIT TEST CASE HERE
+        restaurant = Mockito.spy(restaurant);
+        //Mock getCurrentTime() to 10 minutes after the closingTime
+        Mockito.when(restaurant.getCurrentTime()).thenReturn(restaurant.closingTime.plusMinutes(10));
+        Boolean restaurantStatus = restaurant.isRestaurantOpen();
+        assertFalse(restaurantStatus);
 
     }
 
